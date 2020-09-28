9c253976c47c37ad8764e70fffab95c3e0281bec
rafaelnelli <56029694+rafaelnelli@users.noreply.github.com>
2020-09-28 12:34:00 -0300

    Add files via upload

---
+++ Operacoes.js
@@ -0,0 +1,19 @@
+var x, y, soma, sub, produto, divisao, resto;
+
+x= parseFloat(prompt("Digite o primeiro numero:"));
+y= parseFloat(prompt("Digite o segundo numero:"));
+
+soma=(x+y);
+alert("Soma:"+soma);
+
+sub=(x-y);
+alert("Subtracao:"+sub);
+
+produto=(x*y);
+alert("Produto:"+produto);
+
+divisao=(x/y);
+alert("Divisao:"+divisao);
+
+resto=(x%y);
+alert("Resto:"+resto);
