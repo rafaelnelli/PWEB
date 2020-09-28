c1196da0aef006dc1e46aac1932172304c7e06f6
rafaelnelli <56029694+rafaelnelli@users.noreply.github.com>
2020-09-28 12:34:57 -0300

    Add files via upload

---
+++ Atividade5/Media.js
@@ -0,0 +1,16 @@
+
+    
+    var nome, nota1, nota2, nota3, Media;
+
+    nome = prompt ("Qual o nome do aluno?");
+    nota1= parseFloat(prompt("Digite a nota"));
+    nota2= parseFloat(prompt("Digite a nota"));
+    nota3= parseFloat(prompt("Digite a nota"));
+
+    Media=(nota1+nota2+nota3)/3;
+
+    alert("Sua media é de: "+Media);
+    
+    
+
+ 
