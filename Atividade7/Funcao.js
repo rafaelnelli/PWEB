68b9d67c6eb819fd2c45ab9f9f101d504b13d8a1
rafaelnelli <56029694+rafaelnelli@users.noreply.github.com>
2020-10-19 12:18:42 -0300

    Add files via upload

---
+++ Atividade7/Funcao.js
@@ -0,0 +1,11 @@
+var maior = function(valor1,valor2,valor3){
+    var maio=valor1;
+    if (valor2>=maio)
+        maio= valor2
+    if (valor3>=maio)
+        maio= valor3
+        
+    return maio
+}
+
+alert(maior(1,2,3))
---
+++ Atividade8/Aluno.js
@@ -0,0 +1,16 @@
+var aluno1 = {}
+aluno1["Nome"] = "Rafael Marinelli";
+aluno1["RA"] = 1234;
+
+
+var aluno2 = {
+    Nome:"Rafael Marinelli",
+    RA:1234
+};
+
+var aluno3 = {};
+aluno3.nome= "Rafael Marinelli";
+aluno3.RA = "1234";
+
+console.log(aluno1,aluno2,aluno3)
+
